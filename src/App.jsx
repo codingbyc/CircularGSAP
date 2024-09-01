4c331b4622747dc478c0c40e480ae7ff08b2df7a
codingbyc <artist.chirayu@gmail.com>
2024-09-02 00:01:44 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import "./style.css";
+import { OrbitControls } from "@react-three/drei";
+// import { texture } from 'three/webgpu';
+import Cyl from "./cyl";
+import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+    <Canvas flat camera={{ fov: 35 }}>
+      {/* <OrbitControls /> */}
+      <ambientLight />
+      <Cyl />
+      <EffectComposer>
+        <Bloom
+          mipmapBlur
+          intensity={7.0} // The bloom intensity.
+          luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+      </EffectComposer>
+      </Canvas>
+      <div className="w-full bg-black py-32">
+        <h1 className="text-white text-center">Welcome to My Portfolio.</h1>
+      </div>  
+      </>
+  );
+};
 
-export default App
+export default App;
